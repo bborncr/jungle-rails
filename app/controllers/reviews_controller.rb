0acd04fed340c5917503da6b2a57b6c8c8b316ac
bborncr <bentley@crcibernetica.com>
2017-08-03 23:07:32 +0000

    review posting working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,28 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise "Yay, I'm here!"
+    p = Product.find params[:product_id]
+
+    review_data = review_params
+    @review = p.reviews.new(review_data.merge({
+      user: current_user
+    }))
+    if @review.save
+      redirect_to :back
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :rating,
+      :description,
+      :user_id,
+      :product_id
+    )
   end
 
 end
