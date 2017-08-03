b2d839295b8dd4d8eba54ffdfa900fe9edc1e8b6
bborncr <bentley@crcibernetica.com>
2017-08-03 14:42:50 +0000

    Review model created, validations set

---
+++ app/models/review.rb
@@ -0,0 +1,10 @@
+class Review < ActiveRecord::Base
+
+  belongs_to :product
+
+  validates :product, presence: true
+  validates :user, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
+
+end
